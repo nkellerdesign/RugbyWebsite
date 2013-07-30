651830ecbe803e9a8da93278169cbd9f2dacc992
Nikki Keller <nkeller@nist.gov>
2013-07-30 14:25:07 -0400

    added all other pages I've created
    These are subject to change

---
+++ roster.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Frederick Women's Rugby Club</title>
+</head>
+<body>
+
+<h1>Frederick Women's Rugby Football Club</h1>
+<div id = "menu">
+	<table>
+		<tr>
+			<td>Home</td>
+			<td>History</td>
+			<td>Schedule</td>
+			<td id = "current">Roster</td>
+			<td>Media</td>
+			<td>Contacts</td>
+			<td>Maps</td>
+		</tr>
+	</table>
+</div>
+<div id = "content">
+	<h2>Roster</h2>
+	<table>
+		<tr>
+			<td></td>
+		</tr>
+	</table>
+</div>
+</body>
+</html>
